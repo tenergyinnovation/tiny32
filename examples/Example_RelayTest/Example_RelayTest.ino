486d0f6ca3395d557676ec086e2ab8b8ecfcf9de
Uten Boonliam <45764087+tenergyinnovation@users.noreply.github.com>
2022-04-22 16:58:46 +0700

    Update Example_RelayTest.ino
     * Project      :     Example_RelayTest
     * Description  :     Test Relay on tiny32 board
     * Hardware     :     tiny32
     * Author       :     Tenergy Innovation Co., Ltd.
     * Date         :     19/02/2022
     * Revision     :     1.1
     * Rev1.0       :     Initial
     * Rev1.1       :     Change message status between "ON" and "OFF"

--- examples/Example_RelayTest/Example_RelayTest.ino
+++ examples/Example_RelayTest/Example_RelayTest.ino
@@ -4,7 +4,9 @@
  * Hardware     :     tiny32         
  * Author       :     Tenergy Innovation Co., Ltd.
  * Date         :     19/02/2022
- * Revision     :     1.0
+ * Revision     :     1.1
+ * Rev1.0       :     Initial
+ * Rev1.1       :     Change message status between "ON" and "OFF"
  * website      :     http://www.tenergyinnovation.co.th
  * Email        :     admin@innovation.co.th
  * TEL          :     +66 82-380-3299
@@ -21,12 +23,12 @@ void setup() {
 void loop() {
   
   /**** one beep ****/
-  Serial.printf("Relay => ON\r\n");
+  Serial.printf("Relay => OFF\r\n");
   mcu.Relay(0); //NO disconnect COM  
   vTaskDelay(1000);
 
   /**** two beep ****/
-  Serial.printf("Relay => OFF\r\n");
+  Serial.printf("Relay => ON\r\n");
   mcu.Relay(1); //NO connect COM
   vTaskDelay(1000);
 
